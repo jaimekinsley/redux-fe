d44ae567f245be3a83e8a2d42e22ec2ad41db555
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-11 15:26:04 -0700

    adds router to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import ContactsPage from './contacts/ContactsPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Signup from '../auth/Signup';
+import ContactsPage from './contacts/ContactsPage';
+
 
 export default function App() {
-  return <Signup />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </Router>
+  );
 }
